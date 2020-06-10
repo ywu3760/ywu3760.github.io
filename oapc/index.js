791a9710bbf2ad6ad67343a0a8faec5ea27136a0
ywu3760 <ywu3760@bths.edu>
2020-06-10 02:46:11 -0400

    canvas background time

--- oapc/index.js
+++ oapc/index.js
@@ -1,4 +1,25 @@
-function intialize()
+class Drawing
 {
-    
+    constructor()
+    {
+        for (var i=0, h=0; i<document.body.children.length; i++)
+            h+= document.body.children[i].getBoundingClientRect().height;
+        
+        this.element = document.createElement('canvas');
+        this.context = this.element.getContext('2d');
+        this.contextWidth = this.element.width = window.innerWidth;
+        this.contextHeight = this.element.height = h;
+
+        this.updateBackground();
+    }
+
+    updateBackground()
+    {
+        document.body.style.background = `url(${this.element.toDataURL()})`;
+    }
+}
+
+function init()
+{
+    new Drawing;
 }
